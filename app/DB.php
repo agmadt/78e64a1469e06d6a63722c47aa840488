43e634ecbadea21f4dd7119cd0465e9097ab4365
Agam Adhityo <adhityoagam@gmail.com>
2020-11-28 22:21:04 +0700

    :airplane: able to store sent and unset email

---
+++ app/DB.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App;
+
+use PDO;
+use PDOException;
+
+class DB
+{
+    private $_dbh;
+    private $error;
+
+    private $_stmt;
+
+    public function __construct()
+    {
+        // Set DSN
+        $dsn = $_ENV['DB_CONNECTION'] . ':host=' . $_ENV['DB_HOST'] . ';dbname=' . $_ENV['DB_NAME'];
+        // Set options
+        $options = array(
+            PDO::ATTR_PERSISTENT    => true,
+            PDO::ATTR_ERRMODE       => PDO::ERRMODE_EXCEPTION
+        );
+
+        try {
+            $this->_dbh = new PDO($dsn, $_ENV['DB_USERNAME'], $_ENV['DB_PASSWORD'], $options);
+        } catch (PDOException $e) {
+            $this->error = $e->getMessage();
+        }
+    }
+
+    public function query($query)
+    {
+        $this->_stmt = $this->_dbh->prepare($query);
+    }
+
+    public function bind($param, $value, $type = null)
+    {
+        if (is_null($type)) {
+            switch (true) {
+                case is_int($value):
+                    $type = PDO::PARAM_INT;
+                    break;
+                case is_bool($value):
+                    $type = PDO::PARAM_BOOL;
+                    break;
+                case is_null($value):
+                    $type = PDO::PARAM_NULL;
+                    break;
+                default:
+                    $type = PDO::PARAM_STR;
+            }
+        }
+        $this->_stmt->bindValue($param, $value, $type);
+    }
+
+    public function execute()
+    {
+        return $this->_stmt->execute();
+    }
+
+    public function resultset()
+    {
+        $this->execute();
+        return $this->_stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function single()
+    {
+        $this->execute();
+        return $this->_stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function rowCount()
+    {
+        return $this->_stmt->rowCount();
+    }
+
+    public function lastInsertId()
+    {
+        return $this->dbh->lastInsertId();
+    }
+
+    /**
+     * Transactions allow multiple changes to a database all in one batch.
+     */
+    public function beginTransaction()
+    {
+        return $this->dbh->beginTransaction();
+    }
+
+    public function endTransaction()
+    {
+        return $this->dbh->commit();
+    }
+
+    public function cancelTransaction()
+    {
+        return $this->dbh->rollBack();
+    }
+
+    public function debugDumpParams()
+    {
+        return $this->_stmt->debugDumpParams();
+    }
+}
--- app/controllers/EmailController.php
+++ app/controllers/EmailController.php
@@ -5,6 +5,7 @@ namespace App\Controllers;
 use PHPMailer\PHPMailer\SMTP;
 use PHPMailer\PHPMailer\Exception;
 use PHPMailer\PHPMailer\PHPMailer;
+use App\DB;
 
 /**
  * EmailController class
@@ -20,44 +21,90 @@ class EmailController
     {
         $body = json_decode(file_get_contents('php://input'));
 
-        if (empty($body->emails) || !is_array($body->emails)) {
+        if ($error = $this->validate($body)) {
             header('HTTP/1.1 422 Unprocessable Entity');
             echo json_encode([
-                'message' => 'Problem with request'
+                'message' => $error
             ]);
             return;
         }
 
-        $unsentEmail = [];
-        $uniqueEmails = array_unique($body->emails);
-        foreach ($uniqueEmails as $email) {
-            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                continue;
-            }
-
+        $db = new DB();
+        foreach ($body->emails as $email) {
             try {
                 $mail = new PHPMailer(true);
                 $mail->SMTPAuth = true;
-                $mail->SMTPDebug = SMTP::DEBUG_SERVER;
                 $mail->Host = $_ENV['MAIL_HOST'];
                 $mail->Username = $_ENV['MAIL_USERNAME'];
-                $mail->Password = $_ENV['MAIL_PASSWORD'];
+                $mail->Password = 'asd';
                 $mail->SMTPSecure = $_ENV['MAIL_ENCRYPTION'];
                 $mail->isSMTP();
                 $mail->Port = $_ENV['MAIL_PORT'];
                 $mail->isHTML(true);
                 $mail->setFrom($_ENV['MAIL_FROM'], $_ENV['MAIL_FROM_NAME']);
-                $mail->addAddress($email);
+                $mail->addAddress($email->to);
                 $mail->Subject = 'Here is the subject';
-                $mail->Body    = 'Test message';
+                $mail->Body = 'Test message';
                 $mail->send();
+
+                $db->query('INSERT INTO sent_emails (payload, sent_at) VALUES (:payload, :sent_at)');
+                $db->bind(':payload', json_encode([
+                    'to' => $email->to,
+                    'subject' => $email->subject,
+                    'message' => $email->message
+                ]));
+                $db->bind(':sent_at', date('Y-m-d H:i:s'));
+                $db->execute();
             } catch (Exception $e) {
-                $unsentEmail[] = $email;
+                $db->query('INSERT INTO unsent_emails (payload, failed_at) VALUES (:payload, :failed_at)');
+                $db->bind(':payload', json_encode([
+                    'to' => $email->to,
+                    'subject' => $email->subject,
+                    'message' => $email->message
+                ]));
+                $db->bind(':failed_at', date('Y-m-d H:i:s'));
+                $db->execute();
             }
         }
 
         echo json_encode([
             'message' => 'Emails sent'
         ]);
+    }
+
+    /**
+     * Validate function
+     *
+     * @return mixed
+     */
+    public function validate($request)
+    {
+        if (empty($request->emails) || !isset($request->emails)) {
+            return 'Emails cannot be empty';
+        }
+
+        if (!is_array($request->emails)) {
+            return 'Emails must be an array';
+        }
+
+        foreach ($request->emails as $key => $email) {
+            if (empty($email->to) || !isset($email->to)) {
+                return 'Emails ' . $key . ' `to` ' . 'is required';
+            }
+
+            if (!filter_var($email->to, FILTER_VALIDATE_EMAIL)) {
+                return 'Emails ' . $key . ' `to` ' . 'must be an email';
+            }
+
+            if (empty($email->subject) || !isset($email->subject)) {
+                return 'Emails ' . $key . ' `subject` ' . 'is required';
+            }
+
+            if (empty($email->message) || !isset($email->message)) {
+                return 'Emails ' . $key . ' `message` ' . 'is required';
+            }
+        }
+
+        return null;
     }
 }
